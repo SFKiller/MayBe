0d81714f76a332ff59fcc50096779d22d2fa47aa
qipu <qipu@qiyi.com>
2016-07-27 17:59:03 +0800

    First add

---
+++ MayBe/app/src/main/java/com/kingleoric/maybe/ui/KingLeoricActivity.java
@@ -0,0 +1,18 @@
+package com.kingleoric.maybe.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by qipu on 2016/7/27.
+ */
+
+public class KingLeoricActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
